33283c6c0fba7d560ffe1d76c567a66a89add7b8
proyrb <proyrb@yeah.net>
2025-11-13 10:01:46 +0800

    添加SDRAM接口

--- driver/sdram/sdram.c
+++ driver/sdram/sdram.c
@@ -1,37 +1,23 @@
 #include <sdram.h>
 
-static int sdram_send_command(uint32_t CommandMode, uint32_t Bank,
-                              uint32_t RefreshNum, uint32_t RegVal)
-{
-    uint32_t CommandTarget = 0;
-    FMC_SDRAM_CommandTypeDef Command;
+#define SDRAM_TEST_SIZE 16
 
-    if (Bank == 1)
-    {
-        CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
-    }
-    else if (Bank == 2)
-    {
-        CommandTarget = FMC_SDRAM_CMD_TARGET_BANK2;
-    }
-
-    Command.CommandMode = CommandMode;
-    Command.CommandTarget = CommandTarget;
-    Command.AutoRefreshNumber = RefreshNum;
-    Command.ModeRegisterDefinition = RegVal;
-
-    if (HAL_SDRAM_SendCommand(&hsdram1, &Command, 0x1000) != HAL_OK)
-    {
-        return -1;
-    }
-
-    return 0;
-}
+#define SDRAM_MODEREG_BURST_LENGTH_1 ((SDRAM_UINT16_TYPE)0x0000)
+#define SDRAM_MODEREG_BURST_LENGTH_2 ((SDRAM_UINT16_TYPE)0x0001)
+#define SDRAM_MODEREG_BURST_LENGTH_4 ((SDRAM_UINT16_TYPE)0x0002)
+#define SDRAM_MODEREG_BURST_LENGTH_8 ((SDRAM_UINT16_TYPE)0x0004)
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL ((SDRAM_UINT16_TYPE)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED ((SDRAM_UINT16_TYPE)0x0008)
+#define SDRAM_MODEREG_CAS_LATENCY_2 ((SDRAM_UINT16_TYPE)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3 ((SDRAM_UINT16_TYPE)0x0030)
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD ((SDRAM_UINT16_TYPE)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((SDRAM_UINT16_TYPE)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE ((SDRAM_UINT16_TYPE)0x0200)
 
 void sdram_clear(void)
 {
-    uint8_t *pSRAM = (uint8_t *)SDRAM_ADDR;
-    for (uint32_t i = 0; i < SDRAM_SIZE; i++)
+    SDRAM_UINT8_TYPE *pSRAM = (SDRAM_UINT8_TYPE *)SDRAM_ADDR;
+    for (SDRAM_UINT32_TYPE i = 0; i < SDRAM_SIZE; i++)
     {
         *pSRAM++ = 0;
     }
@@ -39,112 +25,77 @@ void sdram_clear(void)
 
 void sdram_init(void)
 {
-    uint32_t temp;
+    /* 时钟使能命令 */
+    sdram_send_cmd(FMC_SDRAM_CMD_CLK_ENABLE, 1, 1, 0);
+    SDRAM_DELAY_INTERFACE(1);
 
-    /* 1. 时钟使能命令 */
-    sdram_send_command(FMC_SDRAM_CMD_CLK_ENABLE, 1, 1, 0);
+    /* SDRAM全部预充电命令 */
+    sdram_send_cmd(FMC_SDRAM_CMD_PALL, 1, 1, 0);
 
-    /* 2. 延时，至少100us */
-    rt_thread_mdelay(1);
+    /* 自动刷新命令 */
+    sdram_send_cmd(FMC_SDRAM_CMD_AUTOREFRESH_MODE, 1, 8, 0);
 
-    /* 3. SDRAM全部预充电命令 */
-    sdram_send_command(FMC_SDRAM_CMD_PALL, 1, 1, 0);
-    rt_thread_mdelay(1);
+    /* 配置SDRAM模式寄存器 */
+    SDRAM_UINT32_TYPE temp;
+    temp = (SDRAM_UINT32_TYPE)SDRAM_MODEREG_BURST_LENGTH_1 | // 设置突发长度：1
+           SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL |   // 设置突发类型：连续
+           SDRAM_MODEREG_CAS_LATENCY_3 |           // 设置CL值：3
+           SDRAM_MODEREG_OPERATING_MODE_STANDARD | // 设置操作模式：标准
+           SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;   // 设置突发写模式：单点访问
+    sdram_send_cmd(FMC_SDRAM_CMD_LOAD_MODE, 1, 1, temp);
 
-    /* 4. 自动刷新命令 */
-    sdram_send_command(FMC_SDRAM_CMD_AUTOREFRESH_MODE, 1, 8, 0);
-    rt_thread_mdelay(1);
-
-    /* 5. 配置SDRAM模式寄存器 */
-    temp = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1 | // 设置突发长度：1
-           SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL |    // 设置突发类型：连续
-           SDRAM_MODEREG_CAS_LATENCY_3 |            // 设置CL值：3
-           SDRAM_MODEREG_OPERATING_MODE_STANDARD |  // 设置操作模式：标准
-           SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;    // 设置突发写模式：单点访问
-    sdram_send_command(FMC_SDRAM_CMD_LOAD_MODE, 1, 1, temp);
-    rt_thread_mdelay(1);
-
-    /* 6. 设置自刷新频率 */
+    /* 设置自刷新频率 */
     /* SDRAM refresh period / Number of rows）*SDRAM时钟速度 – 20
      * = 64000(64ms) / 8192 * 120MHz - 20
      */
     HAL_SDRAM_ProgramRefreshRate(&hsdram1, 918);
-    rt_thread_mdelay(1);
 
+    /* 清空SDRAM */
     sdram_clear();
 }
 
 void sdram_test(void)
 {
-    uint32_t i;
-    uint8_t *pSRAM;
+    SDRAM_UINT8_TYPE r_data[SDRAM_TEST_SIZE] = {0};
+    SDRAM_UINT8_TYPE t_data[SDRAM_TEST_SIZE] = {0};
+    for (SDRAM_UINT32_TYPE i = 0; i < sizeof(t_data); i++)
+    {
+        t_data[i] = i;
+    }
 
     /* 读SDRAM */
-    rt_lprintf("first read after boot\n");
-    pSRAM = (uint8_t *)SDRAM_ADDR;
-    for (i = 0; i < SDRAM_TEST_SIZE; i++)
+    SDRAM_LOG_INTERFACE("first read after boot\n");
+    sdram_read_data(0, r_data, sizeof(r_data));
+    for (SDRAM_UINT32_TYPE i = 0; i < sizeof(r_data); i++)
     {
-        rt_lprintf("[0x%X:0x%X]\n", i, *pSRAM);
-        pSRAM++;
+        SDRAM_LOG_INTERFACE("[0x%X:0x%X]\n", i, r_data[i]);
     }
 
     /* 写SDRAM */
-    rt_lprintf("write fix value\n");
-    pSRAM = (uint8_t *)SDRAM_ADDR;
-    for (i = 0; i < SDRAM_TEST_SIZE; i++)
-    {
-        *pSRAM = i;
-        pSRAM++;
-    }
+    SDRAM_LOG_INTERFACE("write fix value\n");
+    sdram_write_data(0, t_data, sizeof(t_data));
 
     /* 读SDRAM */
-    rt_lprintf("read after write\n");
-    pSRAM = (uint8_t *)SDRAM_ADDR;
-    for (i = 0; i < SDRAM_TEST_SIZE; i++)
+    SDRAM_LOG_INTERFACE("read after write\n");
+    sdram_read_data(0, r_data, sizeof(r_data));
+    for (SDRAM_UINT32_TYPE i = 0; i < sizeof(r_data); i++)
     {
-        rt_lprintf("[0x%X:0x%X]\n", i, *pSRAM);
-        pSRAM++;
+        SDRAM_LOG_INTERFACE("[0x%X:0x%X]\n", i, r_data[i]);
     }
 
     /* 对SDRAM的数据求反并写入 */
-    rt_lprintf("write rebit value\n");
-    pSRAM = (uint8_t *)SDRAM_ADDR;
-    for (i = 0; i < SDRAM_TEST_SIZE; i++)
+    SDRAM_LOG_INTERFACE("write rebit value\n");
+    for (SDRAM_UINT32_TYPE i = 0; i < sizeof(t_data); i++)
     {
-        *pSRAM = ~*pSRAM;
-        pSRAM++;
+        t_data[i] = ~t_data[i];
     }
+    sdram_write_data(0, t_data, sizeof(t_data));
 
     /* 再次比较SDRAM的数据 */
-    rt_lprintf("read after rebit\n");
-    pSRAM = (uint8_t *)SDRAM_ADDR;
-    for (i = 0; i < SDRAM_TEST_SIZE; i++)
+    SDRAM_LOG_INTERFACE("read after rebit\n");
+    sdram_read_data(0, r_data, sizeof(r_data));
+    for (SDRAM_UINT32_TYPE i = 0; i < sizeof(r_data); i++)
     {
-        rt_lprintf("[0x%X:0x%X]\n", i, *pSRAM);
-        pSRAM++;
+        SDRAM_LOG_INTERFACE("[0x%X:0x%X]\n", i, r_data[i]);
     }
-
-    // /* 测试按字节方式访问, 目的是验证 FSMC_NBL0 、 FSMC_NBL1 口线 */
-    // uint8_t *pBytes;
-    // const uint8_t ByteBuf[4] = {0x55, 0xA5, 0x5A, 0xAA};
-    // pBytes = (uint8_t *)SDRAM_ADDR;
-    // for (i = 0; i < sizeof(ByteBuf); i++)
-    // {
-    //     *pBytes++ = ByteBuf[i];
-    // }
-
-    // /* 比较SDRAM的数据 */
-    // err = 0;
-    // pBytes = (uint8_t *)SDRAM_ADDR;
-    // for (i = 0; i < sizeof(ByteBuf); i++)
-    // {
-    //     if (*pBytes++ != ByteBuf[i])
-    //     {
-    //         err++;
-    //     }
-    // }
-    // if (err > 0)
-    // {
-    //     rt_lprintf("3\n");
-    // }
 }
--- driver/sdram/sdram.h
+++ driver/sdram/sdram.h
@@ -2,31 +2,72 @@
 #define SDRAM_H
 
 /* 导入外部库 */
-#define ADS1118_HAL_INC <stm32h7xx_hal.h>
-#include ADS1118_HAL_INC
-#define ADS1118_BSP_INC <board.h>
-#include ADS1118_BSP_INC
-#define ADS1118_RTOS_INC <rtthread.h>
-#include ADS1118_RTOS_INC
+#define SDRAM_HAL_INC <stm32h7xx_hal.h>
+#include SDRAM_HAL_INC
+#define SDRAM_BSP_INC <board.h>
+#include SDRAM_BSP_INC
+#define SDRAM_RTOS_INC <rtthread.h>
+#include SDRAM_RTOS_INC
 
-#define SDRAM_ADDR      ((uint32_t)0xC0000000)
-#define SDRAM_SIZE      ((uint32_t)(32 * 1024 * 1024))
-#define SDRAM_TEST_SIZE 16
+/* 定义数据类型接口 */
+#define SDRAM_UINT8_TYPE  uint8_t
+#define SDRAM_UINT16_TYPE uint16_t
+#define SDRAM_UINT32_TYPE uint32_t
 
-/* SDRAM配置参数 */
-#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
-#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
-#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
-#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
-#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
-#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
-#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+/* 配置串口日志 */
+#define SDRAM_LOG 1
+#if SDRAM_LOG == 1
+/* 串口日志接口 */
+#define SDRAM_LOG_INTERFACE rt_lprintf
+#else
+#define SDRAM_LOG_INTERFACE(...)
+#endif
+
+/* 提供延时函数 */
+#define SDRAM_DELAY_INTERFACE rt_thread_mdelay
+
+/* SDRAM信息 */
+#define SDRAM_ADDR ((SDRAM_UINT8_TYPE *)(0xC0000000))
+#define SDRAM_SIZE ((SDRAM_UINT32_TYPE)(32 * 1024 * 1024))
+
+/* 接口导出与实现 */
+inline void sdram_send_cmd(SDRAM_UINT32_TYPE cmd_mode, SDRAM_UINT32_TYPE bank,
+                           SDRAM_UINT32_TYPE refresh_cnt,
+                           SDRAM_UINT32_TYPE reg_val)
+{
+    // #warning "Please implement the sdram send cmd function!"
+    FMC_SDRAM_CommandTypeDef cmd;
+    cmd.CommandMode = cmd_mode;
+    if (bank == 1) {
+        cmd.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
+    } else if (bank == 2) {
+        cmd.CommandTarget = FMC_SDRAM_CMD_TARGET_BANK2;
+    }
+    cmd.AutoRefreshNumber      = refresh_cnt;
+    cmd.ModeRegisterDefinition = reg_val;
+    HAL_SDRAM_SendCommand(&hsdram1, &cmd, 10);
+}
+
+inline void sdram_read_data(SDRAM_UINT32_TYPE rel_addr, SDRAM_UINT8_TYPE *buf,
+                            SDRAM_UINT32_TYPE buf_size)
+{
+    // #warning "Please implement the sdram read data function!"
+    HAL_SDRAM_Read_8b(&hsdram1, (SDRAM_UINT32_TYPE *)(SDRAM_ADDR + rel_addr),
+                      buf, buf_size);
+}
+
+inline void sdram_write_data(SDRAM_UINT32_TYPE rel_addr, SDRAM_UINT8_TYPE *buf,
+                             SDRAM_UINT32_TYPE buf_size)
+{
+    // #warning "Please implement the sdram read data function!"
+    HAL_SDRAM_Write_8b(&hsdram1, (SDRAM_UINT32_TYPE *)(SDRAM_ADDR + rel_addr),
+                       buf, buf_size);
+}
 
 void sdram_init(void);
+
 void sdram_test(void);
+
+/*  */
 
 #endif
